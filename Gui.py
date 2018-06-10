934f59350d0d99f59ef4afb70aec7b9a546f1027
ronniemi <ronniemi@post.bgu.ac.il>
2018-06-10 21:14:27 +0300

    interpter

--- Gui.py
+++ Gui.py
@@ -12,7 +12,7 @@ class Gui(tk.Frame):
 
     def __init__(self, master):
         # Initialize window using the parent's constructor
-        tk.Frame.__init__(self, master, width=1000, height=1000)
+        tk.Frame.__init__(self, master)
         # Set the title
         self.master.title('"K Means Clustering"')
 
@@ -24,37 +24,37 @@ class Gui(tk.Frame):
 
         self.file_path_val = tk.StringVar()
         self.file_path_label = tk.Label(self, text='choose file path')
-        self.file_path_label.grid(row=0)
+        self.file_path_label.grid(row=0,column=0)
 
         self.file_path = tk.Entry(self, width=50)
-        self.file_path.grid(row=0, column=1)
+        self.file_path.grid(row=0,column=1)
 
         self.brows = tk.Button(self, text='Browse', command=self.brows_file)
         self.brows.grid(row=0,column=2)
 
         self.Num_of_runs_val = tk.IntVar()
         self.Num_of_runs_label = tk.Label(self, text='Num of runs')
-        self.Num_of_runs_label.grid(row=1)
+        self.Num_of_runs_label.grid(row=1,column=0)
 
         self.Num_of_runs = tk.Entry(self, textvariable=self.Num_of_runs_val)
-        self.Num_of_runs.grid(row=1, column=1)
+        self.Num_of_runs.grid(row=1,column=1)
         self.Num_of_runs_val.set(1)
 
         self.Num_of_clusters_val = tk.IntVar()
         self.Num_of_clusters_label = tk.Label(self, text='Num of clusters k')
-        self.Num_of_clusters_label.grid(row=2)
+        self.Num_of_clusters_label.grid(row=2,column=0)
 
         self.Num_of_clusters = tk.Entry(self, textvariable=self.Num_of_clusters_val)
-        self.Num_of_clusters.grid(row=2, column=1)
+        self.Num_of_clusters.grid(row=2,column=1)
         self.Num_of_clusters_val.set(2)
 
         # The pre_process button
         self.pre_process = tk.Button(self, text='Pre-process', command=self.pre_process)
-        self.pre_process.grid(row=3)
+        self.pre_process.grid(row=3,column=1)
 
         # The cluster button
         self.cluster = tk.Button(self, text='Cluster', state='disabled', command=self.build_model)
-        self.cluster.grid(row=3, column=1)
+        self.cluster.grid(row=3,column=2)
 
     def run(self):
         self.mainloop()
@@ -82,12 +82,11 @@ class Gui(tk.Frame):
             model.k_means(num_of_clusters, num_of_runs)
             #answer = tkMessageBox.askokcancel("K Means Clustering", 'Clustring complited successfully')
             scatter_path = model.plot_scatter()
-            #map_path = model.plot_map()
+            map_path = model.plot_scatter()#model.plot_map()
             self.scatter_img = ImageTk.PhotoImage(Image.open(scatter_path))
-            self.scatter_img_label = tk.Label(self, image=self.scatter_img).pack(side=tk.LEFT)
-            #self.map_img = ImageTk.PhotoImage(Image.open(map_path))
-            #self.map_img_label = tk.Label(self, image=self.map_img).pack(side='bottom', fill='right')
-            #self.map_img.grid(row=4, column=1)
+            self.scatter_img_label = tk.Label(self, image=self.scatter_img).grid(row=4,column=1)
+            self.map_img = ImageTk.PhotoImage(Image.open(map_path))
+            self.map_img_label = tk.Label(self, image=self.map_img).grid(row=4,column=3)
             #answer = tkMessageBox.showinfo("K Means Clustering", 'Clustring complited successfully')
             #if(answer):
              #   self.master.quit()
